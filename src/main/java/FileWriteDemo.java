642c22035e4cbd39efd203b3dfb0336cb6b4b376
joseriveramorales <joserivervmorales@gmail.com>
2022-02-02 22:41:23 -0400

    Save

--- src/main/java/FileWriteDemo.java
+++ src/main/java/FileWriteDemo.java
@@ -2,25 +2,26 @@ import java.io.*;
 public class FileWriteDemo {
     
     public static void main(String[] args) throws IOException{
+        // Use configuration class later to remove code smell -> (Hard coding url's)
         File file = new File("/Users/joserivera/Desktop/Code/JavaIO/demo/src/resources/filesito.txt");
         File file2 = new File("/Users/joserivera/Desktop/Code/JavaIO/demo/src/resources/filesitoVacio.txt");
         FileInputStream fis = null;
         BufferedOutputStream outputStream = null;
 
         String resultado = "";
-        try{
+        try {
             fis = new FileInputStream(file);
             outputStream = new BufferedOutputStream(new FileOutputStream(file2));
            
             byte[] b = fis.readAllBytes();
             outputStream.write(b);
-
             System.out.println("\n Escribi el string -> " + resultado + " <- que lei de filesito.txt en filesitoVacio.txt \n");
 
-            fis.close();
-            outputStream.close();
-        } catch(IOException e){
+        }catch(IOException e){
             e.printStackTrace();
-        }    
+        }finally {
+            if(fis != null) fis.close();
+            if(outputStream != null) outputStream.close();
+        }
     }
 }
