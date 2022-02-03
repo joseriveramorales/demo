99608d467e8e0cc86ecdf669468594ecf89cfa8c
joseriveramorales <joserivervmorales@gmail.com>
2022-02-02 22:05:49 -0400

    Added the FileWriteDemo.java

--- src/main/java/FileWriteDemo.java
+++ src/main/java/FileWriteDemo.java
@@ -3,27 +3,25 @@ public class FileWriteDemo {
     
     public static void main(String[] args) throws IOException{
         File file = new File("/Users/joserivera/Desktop/Code/JavaIO/demo/src/resources/filesito.txt");
+        File file2 = new File("/Users/joserivera/Desktop/Code/JavaIO/demo/src/resources/filesitoVacio.txt");
         FileInputStream fis = null;
+        FileOutputStream fout = null;
+
         String resultado = "";
         try{
             fis = new FileInputStream(file);
-            System.out.println("Total file size to read (in bytes) " + fis.available());
+            fout = new FileOutputStream(file2);
+           
             int content;
             while((content = fis.read()) != -1){
-                System.out.println((char) content);
                 resultado += ((char) content);
+                fout.write(content);
             }
-            System.out.println(resultado);
+            System.out.println("Escribi el string -> " + resultado + " <- que lei de filesito.txt en filesitoVacio.txt");
+
         } catch(IOException e){
             e.printStackTrace();
-        } finally {
-            try {
-                if(fis != null)
-                    fis.close();
-            } catch( IOException ex){
-                ex.printStackTrace();
-            }
-        }
+        } 
     }
     
 }
